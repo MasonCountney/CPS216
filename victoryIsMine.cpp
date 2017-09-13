6b46f70d5fcb9469d6ffc68fa7519809ac5fa609
Mason Countney <masoncount@gmail.com>
2017-09-13 12:42:52 -0500

    Create victoryIsMine.cpp

---
+++ victoryIsMine.cpp
@@ -0,0 +1,47 @@
+// victoryIsMine.cpp
+// Mason Countney
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+void top(string input);
+void bottom(string input);
+void phrase(string input);
+
+
+int main()
+{
+
+	string input;
+
+	cout << "Enter phrase here: ";
+	getline(cin, input);
+
+	phrase(input);
+
+    return 0;
+}
+
+void top(string input) {
+	for (int i = 0; i < (input.length() + 6); i++)
+		cout << "/";
+
+	cout << endl;
+}
+
+void bottom(string input) {
+	for (int i = 0; i < (input.length() + 6); i++)
+		cout << "\\";
+
+	cout << endl;
+}
+
+void phrase(string input) {
+	top(input);
+	cout << "|| " << input << " ||" << endl;
+	bottom(input);
+}
